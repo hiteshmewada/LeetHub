11d3d665789d05738f193a027f0ca33eb4b8627e
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-05-08 11:27:56 +0530

    Added solution - LeetHub

---
+++ Nth item through sum - GFG/nth-item-through-sum.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int nthItem(int L1, int L2, int A[], int B[], int N)
+    {
+        // code here
+        set<int>ans;
+        for(int i=0;i<L1;i++){
+            for(int j=0;j<L2;j++){
+                ans.insert(A[i]+B[j]);
+            }
+        }
+        for(auto x:ans){
+            N--;
+            if(N==0) return x;
+        }
+        // return ans[N-1];
+        return  -1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int L1, L2, N;
+        cin>>L1>>L2;
+        int A[L1], B[L2];
+        for(int i = 0;i < L1;i++)
+            cin>>A[i];
+        for(int i = 0;i < L2;i++)
+            cin>>B[i];
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.nthItem(L1, L2, A, B, N)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
