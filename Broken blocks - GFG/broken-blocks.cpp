82ede415a69b85ad00efeed9e717b8a1cc6815dc
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-05-14 17:41:28 +0530

    Added solution - LeetHub

--- Broken blocks - GFG/broken-blocks.cpp
+++ Broken blocks - GFG/broken-blocks.cpp
@@ -5,32 +5,17 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
 public:
-int ans=-1;
-int helper(vector<vector<int>>&v,vector<vector<int>>&dp,int x,int y){
-    int n=v.size(),a=v[0].size();
-    if(x>=n or y>=a or x<0 or y<0 or v[x][y]==-1) {
-        // ans=max(ans,k);
-        // cout<<x<<" "<<y<<endl;
-        return 0;
-    }
-    if(dp[x][y]!=-1) {
-         return dp[x][y];
-    }
-    int b=v[x][y],c=0,d=0,e=0;
-    c=helper(v,dp,x+1,y);
-    d=helper(v,dp,x+1,y+1);
-    e=helper(v,dp,x+1,y-1);
-    return dp[x][y]=max(c,max(d,e))+v[x][y];
-}
-int MaxGold(vector<vector<int>>&v){
+int MaxGold(vector<vector<int>>&m){
     // Code here
-    int n=v.size(),a=v[0].size(),c=0;
-    vector<vector<int>>dp(n+1,vector<int>(a+1,-1));
-    for(int i=0;i<a;i++){
-        ans=max(ans,helper(v,dp,0,i));
-        // cout<<ans<<endl;
+    int n=m.size(),a=m[0].size();
+    for(int i=n-2;i>=0;i--){
+        for(int j=0;j<a;j++){
+            if(m[i][j]!=-1){
+                m[i][j]+=max({j>0?m[i+1][j-1]:0,m[i+1][j],j+1<a?m[i+1][j+1]:0,0});
+            }
+        }
     }
-    return ans;
+    return max(0,*max_element(m[0].begin(),m[0].end()));
 }
 };
 
