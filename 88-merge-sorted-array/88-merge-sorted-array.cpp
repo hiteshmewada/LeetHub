cf53974788484096cbd053ff85053f470f18def4
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-06-05 12:32:56 +0530

    Time: 8 ms (14.69%), Space: 9 MB (72.65%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
+    
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int j=0,i=0;
-        vector<int>ans;
-        while(i<m and j<n){
-            if(nums1[i]<nums2[j]) {
-                ans.push_back(nums1[i]);i++;
+        int gap=ceil((float)(n+m)/2),j;
+        while(gap>0){
+            int i=0,j=gap;
+            while(j<(n+m)){
+                if(j<m and nums1[i]>nums1[j]) swap(nums1[i],nums1[j]);
+                else if(j>=m and i<m and nums1[i]>nums2[j-m])
+                    swap(nums1[i],nums2[j-m]);
+                else if(j>=m and i>=m and nums2[i-m]>nums2[j-m])
+                    swap(nums2[i-m],nums2[j-m]);
+                i++;j++;
             }
+            if(gap==1) gap=0;
             else{
-                ans.push_back(nums2[j]);j++;
+                gap=ceil((float)gap/2);
             }
         }
-        while(i<m) {
-            ans.push_back(nums1[i]);i++;
-        }
-        while(j<n) {
-            ans.push_back(nums2[j]);j++;
-        }
-        nums1=ans;
-//         for(int i=0;i<nums1.size();i++){
-//             if(nums1[i]>=nums2[j]){
-                
-//             }
-//         }
+        j=0;
+        for(int i=m;i<(n+m);i++) nums1[i]=nums2[j++];
     }
 };
