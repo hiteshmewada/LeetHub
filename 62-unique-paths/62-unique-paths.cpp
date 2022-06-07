d546cd65f6ea1ddd5873f0a8b9cee1fb9e0cfe68
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-06-08 00:11:50 +0530

    Time: 0 ms (100.00%), Space: 6 MB (73.64%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        long long dp[m][n];
-        memset(dp,0,sizeof(dp));
-        for(int i=0;i<m;i++) dp[i][0]=1;
-        for(int i=1;i<n;i++) dp[0][i]=1;
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]+=dp[i-1][j]+dp[i][j-1];
-            }
+        double ans=1;
+        int N=n+m-2;
+        int r=n-1;
+        for(int i=1;i<=r;i++){
+            ans=(ans*(N-r+i))/i;
         }
-        return dp[m-1][n-1];
+        return (int)ans;
     }
 };
