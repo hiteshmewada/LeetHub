02f04e2590a57836cc7f47e33312338252106f2e
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-06-14 17:23:45 +0530

    Added solution - LeetHub

--- String formation from substring - GFG/string-formation-from-substring.cpp
+++ String formation from substring - GFG/string-formation-from-substring.cpp
@@ -7,36 +7,34 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
-	int gcd(int a,int b){
-	   // if(b==0) return a;
-	   // return (b,b%a);
-	   // return (b==0)?a:gcd(b,b%a);
-	   return (b==0)?a:gcd(b,a%b);
+	void computeLPS(vector<int>&v,int n,string s){
+	    int len=0,i=1;
+	    v[0]=0;
+	    while(i<n){
+	        if(s[i]==s[len]){
+	            len++;
+	            v[i]=len;
+	            i++;
+	        }
+	        else{
+	            if(len!=0){
+	                len=v[len-1];
+	            }
+	            else{
+	                v[i]=0;
+	                i++;
+	            }
+	        }
+	    }
 	}
 	int isRepeat(string s)
 	{
 	    // Your code goes here
-	    unordered_map<char,int>mp;
-	    int sum=0;
-	    for(int i=0;i<s.size();i++) mp[s[i]]++;
-	    int hcf=mp[s[0]];
-	    for(auto x:mp) {
-	        hcf=gcd(hcf,x.second);
-	        sum+=x.second;
-	    }
-	    if(hcf==1)
-         return 0;
-         hcf=sum/hcf;
-         string  temp;
-            int i;
-            for(i=0;i<hcf;i++) temp+=s[i];
-            int k=0;
-            for(i;i<s.size();i++){
-                if(s[i]!=s[k]) return 0;
-                k++;
-                k%=hcf;
-            }
-            return 1;
+	    int n=s.size();
+	    vector<int> lps(n);
+	    computeLPS(lps,n,s);
+	    int len=lps[n-1];
+	    return (len>0 and n%(n-len)==0)?1:0;
 	}
 };
 
