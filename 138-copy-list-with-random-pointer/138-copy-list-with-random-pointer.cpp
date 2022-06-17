c2059aee01651f0e0875ddb127460ef571f41bb8
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-06-17 17:08:02 +0530

    Time: 15 ms (42.07%), Space: 11.3 MB (60.62%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,18 +17,45 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*,Node*>mp;
-        Node *cur=head;
-        while(cur){
-            mp[cur]=new Node(cur->val);
-            cur=cur->next;
+        Node *iter=head,*front=head;
+        
+        // First round make copy of each node and link them together side-by-side in a single list
+        while(iter){
+            front=iter->next;
+            Node *copy=new Node(iter->val);
+            iter->next=copy;
+            copy->next=front;
+            iter=front;
         }
-        cur=head;
-        while(cur){
-            mp[cur]->next=mp[cur->next];
-            mp[cur]->random=mp[cur->random];
-            cur=cur->next;
+        
+        // Second round: assign random pointers for the copy nodes 
+        
+        iter=head;
+        while(iter){
+            if(iter->random){
+                iter->next->random=iter->random->next;
+            }
+            iter=iter->next->next;
         }
-        return mp[head];
+        
+        // Third round: restore the original list and extract the copy list
+        
+        iter=head;
+        Node *pseudoHead=new Node(0);
+        Node *copy=pseudoHead;
+        
+        while(iter){
+            front=iter->next->next;
+            
+            // extract copy
+            copy->next=iter->next;
+            
+            // restore original list
+            iter->next=front;
+            
+            copy=copy->next;
+            iter=front;
+        }
+        return pseudoHead->next;
     }
 };
