c3693eddb957a4a8fd2b1f74e178fbfe2526313f
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-06-18 10:06:47 +0530

    Time: 812 ms (12.83%), Space: 24.2 MB (20.68%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n=nums.size();
-       vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
+        unordered_map<int,int>mp;
+        int i=0;
+        for(auto x:nums) {
+            mp[x]=i;
+            i++;
+        }
+        vector<vector<int>>ans;
         for(int i=0;i<n-2;i++){
-            if(i==0 or (i>0 and nums[i]!=nums[i-1])){
-                int lo=i+1,hi=n-1,sum=0-nums[i];
-                while(lo<hi){
-                    int temp=nums[lo]+nums[hi];
-                    if(temp==sum){
-                        vector<int>v;
-                        v.push_back(nums[i]);
-                        v.push_back(nums[lo]);
-                        v.push_back(nums[hi]);
-                        ans.push_back(v);
-                        while(lo+1<n and nums[lo]==nums[lo+1]) lo++;
-                        while(hi-1>=0 and nums[hi]==nums[hi-1]) hi--;
-                        lo++;hi--;
-                    }
-                    else if(temp>sum) hi--;
-                    else lo++;
+            for(int j=i+1;j<n-1;j++){
+                int req=-1*(nums[i]+nums[j]);
+                if(mp.find(req)!=mp.end() and mp[req]>j){
+                    vector<int>v;
+                    v.push_back(nums[i]);
+                    v.push_back(nums[j]);
+                    v.push_back(req);
+                    ans.push_back(v);
                 }
+                j=mp[nums[j]];
             }
+            i=mp[nums[i]];
         }
         return ans;
     }
