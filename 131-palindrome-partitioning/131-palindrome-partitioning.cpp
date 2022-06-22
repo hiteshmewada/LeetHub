e225d0e8c4cbaa3ef0103d0a8badd6761ef89b82
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-06-22 17:51:11 +0530

    Time: 204 ms (48.29%), Space: 53.5 MB (70.43%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -7,24 +7,26 @@ public:
         }
         return true;
     }
-    void f(string s,vector<string>&v, vector<vector<string>>&ans,int i){
+    void f(string s,vector<string>&v, vector<vector<string>>&ans,int i,vector<vector<bool>> &dp){
         if(i==s.size()) {
                 ans.push_back(v);
                 // v.push_back(s[i]);
             return;
         }
         for(int j=i;j<s.size();j++){
-            if(palin(s,i,j)){
+            if(s[i]==s[j] and ((j-i)<=1 or dp[i+1][j-1])){
+                dp[i][j]=true;
                 v.push_back(s.substr(i,j-i+1));
-                f(s,v,ans,j+1);
+                f(s,v,ans,j+1,dp);
                 v.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
         vector<string>v;
+        vector<vector<bool>> dp(s.size(),vector<bool>(s.size(),false));
         vector<vector<string>>ans;
-        f(s,v,ans,0);
+        f(s,v,ans,0,dp);
         return ans;
     }
 };
