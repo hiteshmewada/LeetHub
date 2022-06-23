dcba714090583b6dd65d7756329daef9a3070940
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-06-23 15:28:35 +0530

    Time: 9 ms (63.09%), Space: 7.6 MB (37.03%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -22,23 +22,30 @@ public:
         }
         return true;
     }
-    void f(vector<string>&v,int n,int col){
+    void f(vector<string>&v,int n,int col,vector<int>&leftRow,vector<int>&lowerDiagonal,vector<int>&upperDiagonal){
         if(col==n){
             ans.push_back(v);return;
         }
         for(int row=0;row<n;row++){
-            if(isSafe(row,col,v,n)){
+            if(leftRow[row]==0 and lowerDiagonal[row+col]==0 and upperDiagonal[n-1+row-col]==0){
+                leftRow[row]=1;
+                lowerDiagonal[row+col]=1;
+                upperDiagonal[n-1+row-col]=1;
                 v[row][col]='Q';
-                f(v,n,col+1);
+                f(v,n,col+1,leftRow,lowerDiagonal,upperDiagonal);
                 v[row][col]='.';
+                leftRow[row]=0;
+                lowerDiagonal[row+col]=0;
+                upperDiagonal[n-1+row-col]=0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<string>v(n);
+        vector<int>leftRow(n,0),upperDiagonal(2*n-1,0),lowerDiagonal(2*n-1,0);
         string s(n,'.');
         for(int i=0;i<n;i++) v[i]=s;
-        f(v,n,0);
+        f(v,n,0,leftRow,lowerDiagonal,upperDiagonal);
         return ans;
     }
 };
