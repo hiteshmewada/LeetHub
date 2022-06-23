18a58b1eed2dbb315737c69af00233c27f0c14e6
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-06-23 12:36:41 +0530

    Added solution - LeetHub

--- Count possible ways to construct buildings - GFG/count-possible-ways-to-construct-buildings.cpp
+++ Count possible ways to construct buildings - GFG/count-possible-ways-to-construct-buildings.cpp
@@ -5,31 +5,21 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
 	public:
-	long long mod=1000000007;
-	map<pair<long long,char>,long long>dp;
-	bool check(string s){
-	    for(int i=0;i<s.size()-1;i++){
-	        if(s[i]==s[i+1] and s[i]=='B') return false;
-	    }
-	    return true;
-	}
-	int f(long long n,char c,int i){
-	    if(i==n){
-	        return dp[{i,c}]=1;
-	    }
-	    if(dp.find({i,c})!=dp.end()) return dp[{i,c}];
-	    if(c=='B') return dp[{i,c}]=f(n,'S',i+1)%mod;
-	    return dp[{i,c}]=(long long )((f(n,'B',i+1)%mod)+f(n,'S',i+1)%mod);
-	}
 	int TotalWays(int N)
 	{
 	    // Code here
-	   // string s;
-	   // memset(dp,-1,sizeof(dp));
-	    long long ans=f(N,'B',1)%mod+f(N,'S',1)%mod;
-	    ans%=mod;
-	    ans*=ans;
-	    return (int)(ans%mod);
+	    if(N==0) return 0;
+	    if(N<3) return (N+1)*(N+1);
+	    long long pre=3;
+	    long long pre_pre=2;
+	    long long ans=0;
+	    long long mod=1e9+7;
+	    for(int i=3;i<=N;i++){
+	        ans=(pre+pre_pre)%mod;
+	        pre_pre=pre;
+	        pre=ans;
+	    }
+	    return ((ans%mod)*(ans%mod))%mod;
 	}
 };
 
