3c3e93ad8acb5733dff9679bf5d9c5a6efb2b88a
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-07-01 14:46:46 +0530

    Time: 22 ms (62.58%), Space: 13.7 MB (72.83%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
+    void kSelection(vector<pair<int,int>>&v, int st, int end, int k){
+        if(st>=end) return;
+        auto pv=v[end];
+        int i=st,j=st;
+        while(i<end){
+            if(v[i].second>pv.second) swap(v[i++],v[j++]);
+            else i++;
+        }
+        swap(v[j],v[end]);
+        int num=j-st-1;
+        if(num==k) return;
+        else if(num<k) kSelection(v,j+1,end,k-num);
+        else kSelection(v,st,j-1,k);
+    }
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n=nums.size();
-        unordered_map<int,int>mp;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        for(auto x:nums) mp[x]++;
-        for(auto x:mp){
-            pq.push({x.second,x.first});
-            while(pq.size()>k) pq.pop();
-        }
         vector<int>ans;
-        while(pq.size()){
-            ans.push_back(pq.top().second);
-            pq.pop();
+        unordered_map<int,int>mp;
+        vector<pair<int,int>>v;
+        for(auto x:nums) mp[x]++;
+        for(auto x:mp) v.push_back({x.first,x.second});
+        kSelection(v,0,v.size()-1,k);
+        int i=0;
+        while(k and i<v.size()){
+            ans.push_back(v[i].first);
+            k--;
+            i++;
         }
         return ans;
     }
