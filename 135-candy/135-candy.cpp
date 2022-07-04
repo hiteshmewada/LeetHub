4463eeae523a725b85de1b6c3902adbb22a23c62
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-07-04 18:06:22 +0530

    Time: 33 ms (52.60%), Space: 17.1 MB (96.69%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
+    int count(int n){
+        return (n*(n+1))/2;
+    }
     int candy(vector<int>& r) {
         int ans=0,n=r.size();
-        vector<int>pre(n,0);
-        pre[0]=1;
+        if(r.size()<=1) return r.size();
+        int up=0,down=0,oldSlope=0;
         for(int i=1;i<n;i++){
-            if(r[i-1]<r[i]) pre[i]=pre[i-1]+1;
-            else pre[i]=1;
+            int newSlope=(r[i]>r[i-1])?1:(r[i]<r[i-1]?-1:0);
+            if((oldSlope>0 and newSlope==0) or (oldSlope<0 and newSlope>=0)){
+                ans+=count(up)+count(down)+max(up,down);
+                up=0;down=0;
+            }
+            if(newSlope>0) up++;
+            else if(newSlope<0) down++;
+            else ans++;
+            oldSlope=newSlope;
         }
-        for(int i=n-2;i>=0;i--){
-            if(r[i+1]<r[i]) pre[i]=max(pre[i+1]+1,pre[i]);
-        }
-        for(auto x:pre) ans+=x;
+        ans+=count(up)+count(down)+max(up,down)+1;
         return ans;
     }
 };
