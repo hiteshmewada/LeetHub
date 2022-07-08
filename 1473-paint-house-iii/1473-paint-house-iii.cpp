4157b273eb832c7358c2efb636adaddd8ae92e67
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-07-08 23:56:01 +0530

    Time: 79 ms (85.64%), Space: 10.7 MB (92.29%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -1,55 +1,32 @@
 class Solution {
 public:
-    // Assign the size as per maximum value for different params
+    // index,count,color
     int memo[100][100][21];
-    // Maximum cost possible plus 1
-    const int MAX_COST = 1000001;
-    
-    int findMinCost(vector<int>& houses, vector<vector<int>>& cost, int targetCount, int currIndex,
-                    int neighborhoodCount, int prevHouseColor) {
-        if (currIndex == houses.size()) {
-            // If all houses are traversed, check if the neighbor count is as expected or not
-            return neighborhoodCount == targetCount ? 0 : MAX_COST;
+    const int max_cost=1000001;
+    int findMinCost(vector<int>& houses, vector<vector<int>>& cost, int target,int curIndex, int neighborhoodCount, int prevHouseColor){
+        if(curIndex==houses.size()){
+            return neighborhoodCount==target?0:max_cost;
         }
-        
-        if (neighborhoodCount > targetCount) {
-            // If the neighborhoods are more than the threshold, we can't have target neighborhoods
-            return MAX_COST;
+        if(neighborhoodCount>target) return max_cost;
+        if(memo[curIndex][neighborhoodCount][prevHouseColor]!=-1)
+            return memo[curIndex][neighborhoodCount][prevHouseColor];
+        int minCost=max_cost;
+        if(houses[curIndex]){
+            int newNeighborHoodCount=neighborhoodCount+(houses[curIndex]!=prevHouseColor); minCost=findMinCost(houses,cost,target,curIndex+1,newNeighborHoodCount,houses[curIndex]);
         }
-        
-        // We have already calculated the answer so no need to go into recursion
-        if (memo[currIndex][neighborhoodCount][prevHouseColor] != -1) {
-            return memo[currIndex][neighborhoodCount][prevHouseColor];
-        }
-        
-        int minCost = MAX_COST;
-        // If the house is already painted, update the values accordingly
-        if (houses[currIndex]) {
-            int newNeighborhoodCount = neighborhoodCount + (houses[currIndex] != prevHouseColor);
-            minCost = 
-                findMinCost(houses, cost, targetCount, currIndex + 1, newNeighborhoodCount, houses[currIndex]);
-        } else {
-            int totalColors = cost[0].size();
-            
-            // If the house is not painted, try every possible color and store the minimum cost
-            for (int color = 1; color <= totalColors; color++) {
-                int newNeighborhoodCount = neighborhoodCount + (color != prevHouseColor);
-                int currCost = cost[currIndex][color - 1]
-                    + findMinCost(houses, cost, targetCount, currIndex + 1, newNeighborhoodCount, color);
-                minCost = min(minCost, currCost);
+        else{
+            int totalColors=cost[0].size();
+            for(int color=1;color<=totalColors;color++){
+                int newNeighborHoodCount=neighborhoodCount+(color!=prevHouseColor); 
+                int currCost=cost[curIndex][color-1] + findMinCost(houses,cost,target,curIndex+1,newNeighborHoodCount,color);
+                minCost=min(minCost,currCost);
             }
         }
-        
-        // Return the minimum cost and also storing it for future reference (memoization)
-        return memo[currIndex][neighborhoodCount][prevHouseColor] = minCost;
+        return memo[curIndex][neighborhoodCount][prevHouseColor]=minCost;
     }
-    
     int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
-        // Marking all values to -1 to denote that we don't have the answer ready for these params yet
-        memset(memo, -1, sizeof(memo));
-        int answer = findMinCost(houses, cost, target, 0, 0, 0);
-        
-        // Return -1 if the answer is MAX_COST as it implies no answer possible
-        return answer == MAX_COST ? -1 : answer;
+        memset(memo,-1,sizeof(memo));
+        int ans=findMinCost(houses,cost,target,0,0,0);
+        return ans==max_cost?-1:ans;
     }
 };
