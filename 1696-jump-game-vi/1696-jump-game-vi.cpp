3051a52b7a4cb4cbb2d1420c54c32568f2aa67a7
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-07-09 15:59:46 +0530

    Time: 355 ms (39.85%), Space: 80.7 MB (74.64%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -4,10 +4,15 @@ public:
     int maxResult(vector<int>& nums, int k) {
         int n=nums.size();
         vector<int>dp(n,INT_MIN);
-        multiset<int>s({dp[0]=nums[0]});
+        dp[0]=nums[0];
+        deque<int>q{0};
         for(int i=1;i<n;i++){
-           if(i>k) s.erase(s.find(dp[i-k-1]));
-            s.insert(dp[i]=*rbegin(s)+nums[i]);
+           if(q.front()<(i-k)) q.pop_front(); // can't reach current index from index stored in q   
+            dp[i]=nums[i]+dp[q.front()];   //update max score for current index
+            // pop indices which won't be ever chosen in the future
+            while(!q.empty() and dp[q.back()]<=dp[i]) 
+                    q.pop_back();
+            q.push_back(i);
         }
         return dp.back();
     }
