9b09c49400f2025f93860edf46847f50607ea685
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-07-09 12:43:13 +0530

    Time: 162 ms (38.42%), Space: 18.6 MB (24.74%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -5,35 +5,37 @@ public:
     const int max_cost=1000001;
     
     int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
-        int memo[m][target+1][n];
-        memset(memo,max_cost,sizeof(memo));
+        vector<vector<int>>prevMemo(target+1,vector<int>(n,max_cost));
         for(int color=1;color<=n;color++){
-            if(houses[0]==color) memo[0][1][color-1]=0;
+            if(houses[0]==color) prevMemo[1][color-1]=0;
             else if(!houses[0])
-                memo[0][1][color-1]=cost[0][color-1];
+                prevMemo[1][color-1]=cost[0][color-1];
         }
         for(int house=1;house<m;house++){
+            // new memo
+            vector<vector<int>>memo(target+1,vector<int>(n,max_cost));
             for(int neighborhoods=1;neighborhoods<=min(target,house+1);neighborhoods++){
                 for(int color=1;color<=n;color++){
                     if(houses[house] and color!=houses[house]) continue;
                    int curCost=max_cost;
                     for(int prevColor=1;prevColor<=n;prevColor++){
                         if(prevColor!=color){
-                            curCost=min(curCost,memo[house-1][neighborhoods-1][prevColor-1]);
+                            curCost=min(curCost,prevMemo[neighborhoods-1][prevColor-1]);
                         }
                         else{
-                            curCost=min(curCost,memo[house-1][neighborhoods][prevColor-1]);
+                            curCost=min(curCost,prevMemo[neighborhoods][prevColor-1]);
                         }
                     }
                     // cost of painting house
                     int costToPaint=houses[house]?0:cost[house][color-1];
-                    memo[house][neighborhoods][color-1]=curCost+costToPaint;
+                    memo[neighborhoods][color-1]=curCost+costToPaint;
                  }
             }
+            prevMemo=memo;
         }
         int minCost=max_cost;
         for(int color=1;color<=n;color++){
-            minCost=min(minCost,memo[m-1][target][color-1]);
+            minCost=min(minCost,prevMemo[target][color-1]);
         }
         return minCost==max_cost?-1:minCost;
     }
