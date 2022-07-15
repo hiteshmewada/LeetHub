d396bbdb97fb59ba6e566f6c187dded7bfd693a8
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-07-15 21:15:16 +0530

    Time: 36 ms (35.70%), Space: 27.4 MB (8.86%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     vector<vector<int>>grid;
-    vector<vector<bool>>vis;
-    int area(int r, int c){
-        if(r<0 or r>=grid.size() or c<0 or c>=grid[0].size() or vis[r][c]==true or grid[r][c]==0) return 0;
-        vis[r][c]=true;
-        return (1+area(r+1,c)+area(r-1,c)+area(r,c+1)+area(r,c-1));
-    }
+    
     int maxAreaOfIsland(vector<vector<int>>& gri) {
         grid=gri;
         int n=grid.size(),a=grid[0].size();
-        vis.resize(n,{false});
-        for(int i=0;i<n;i++) vis[i].resize(a,{false});
+        bool vis[n][a];
+        memset(vis,false,sizeof(vis));
+        int dx[4]={1,-1,0,0},dy[4]={0,0,1,-1};
         int ans=0;
         for(int r=0;r<n;r++){
             for(int c=0;c<a;c++){
-                ans=max(ans,area(r,c));
+                if(grid[r][c]==1 and !vis[r][c]){
+                    int shape=0;
+                    stack<pair<int,int>>st;
+                    st.push({r,c});
+                    vis[r][c]=true;
+                    while(!st.empty()){
+                        pair<int,int> node=st.top();
+                        st.pop();
+                        int r1=node.first,c1=node.second;
+                        shape++;
+                        for(int k=0;k<4;k++){
+                            int nr=r1+dx[k],nc=c1+dy[k];
+                            if(nr>=0 and nr<n and nc<a and nc>=0 and                                    grid[nr][nc]==1 and !vis[nr][nc]){
+                                st.push({nr,nc});
+                                vis[nr][nc]=true;
+                            }
+                        }
+                    }
+                    ans=max(ans,shape);
+                }
             }
         }
         return ans;
