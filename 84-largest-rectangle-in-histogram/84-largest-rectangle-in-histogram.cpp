8a4cc19471384fddfd975c9a7efcc9120c91dea7
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-07-20 13:26:13 +0530

    Time: 285 ms (48.25%), Space: 81.4 MB (45.31%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -5,22 +5,18 @@ public:
         int ans=INT_MIN;
         stack<int>st;
         vector<int>leftSmall(n,0),rightSmall(n,0);
-        for(int i=0;i<n;i++){
-            while(!st.empty() and h[st.top()]>=h[i]) st.pop();
-            if(st.empty()) leftSmall[i]=0;
-            else leftSmall[i]=st.top()+1;
+        for(int i=0;i<=n;i++){
+            while(!st.empty() and (i==n or h[st.top()]>=h[i])){
+                int height=h[st.top()];
+                st.pop();
+                int width;
+                if(st.size()==0) width=i;
+                else width=i-st.top()-1;
+                ans=max(ans,height*width);
+            }
             st.push(i);
         }
-        while(st.size()) st.pop();
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() and h[st.top()]>=h[i]) st.pop();
-            if(st.empty()) rightSmall[i]=n-1;
-            else rightSmall[i]=st.top()-1;
-            st.push(i);
-        }
-        for(int i=0;i<n;i++){
-            ans=max(ans,h[i]*(rightSmall[i]-leftSmall[i]+1));
-        }
+        
         return ans;
     }
 };
