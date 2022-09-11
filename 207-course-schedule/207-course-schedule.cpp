87e78ef86074ede50158b9f66d44dee56fafb2f2
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-09-11 17:55:23 +0530

    Time: 23 ms (88.44%), Space: 14.1 MB (51.15%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<vector<int>>adj(n,vector<int>());
+        vector<int>deg(n,0);
+        for(auto x:pre){
+            adj[x[1]].push_back(x[0]);
+            deg[x[0]]++;
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(deg[i]==0) q.push(i);
+        }
+        while(q.size()){
+            int cur=q.front();n--;
+            q.pop();
+            for(auto x:adj[cur]){
+                if(--deg[x]==0) q.push(x);
+            }
+        }
+        return n==0;
+    }
+};
