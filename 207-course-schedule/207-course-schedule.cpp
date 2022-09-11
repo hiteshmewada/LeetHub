8471d2c360931aa435a110fe6ca4bbe2983836e3
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-09-11 18:15:53 +0530

    Time: 48 ms (24.85%), Space: 14.3 MB (39.67%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    bool isCycle(int cur, vector<int>adj[],vector<int>&vis ){
-        if(vis[cur]==1) return true;
-        if(vis[cur]==0){
-            vis[cur]=1;
-            for(auto x:adj[cur]){
-                if(isCycle(x,adj,vis)) return true;
+    bool isCycle(int cur,int par, vector<int>adj[],vector<int>&vis, vector<int>&dfsvis ){
+       vis[cur]=1;
+        dfsvis[cur]=1;
+        for(auto x:adj[cur]){
+            if(!vis[x]){
+                if(isCycle(x,cur,adj,vis,dfsvis)) return true;
             }
+            else if(dfsvis[x]) return true;
         }
-        vis[cur]=2;
+        dfsvis[cur]=0;
         return false;
     }
     bool canFinish(int n, vector<vector<int>>& pre) {
@@ -17,8 +18,10 @@ public:
             adj[x[1]].push_back(x[0]);
         }
         vector<int>vis(n,0);
+        vector<int>dfsvis(n,0);
         for(int i=0;i<n;i++){
-            if(isCycle(i,adj,vis)) return false;
+            if(!vis[i])
+            if(isCycle(i,-1,adj,vis,dfsvis)) return false;
         }
         
         return true;
