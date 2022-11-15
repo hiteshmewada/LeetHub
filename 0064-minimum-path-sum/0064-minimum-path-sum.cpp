7035f5bb98c02209917902d2dedcd91b8716e664
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-11-15 17:24:42 +0530

    Time: 6 ms (98.24%), Space: 9.8 MB (81.42%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    
-//     int f(vector<vector<int>>& grid, int i, int j){
-//         if(i==grid.size() or j==grid[0].size()) return 1e9;
-//         if(i==grid.size()-1 and j==grid[0].size()-1) return grid[i][j];
-//         if(dp[i][j]!=-1) return dp[i][j];
-        
-//     }
     int minPathSum(vector<vector<int>>& grid) {
-        
         int n=grid.size(),m=grid[0].size();
-        int dp[n][m];
-        
-        memset(dp,0,sizeof(dp));
+        // int dp[n][m];
+        // memset(dp,0,sizeof(dp));
+        vector<int>nxt(m,0);
         for(int i=n-1;i>=0;i--){
+            vector<int>cur(m,0);
             for(int j=m-1;j>=0;j--){
-                if(i==n-1 and j==m-1) dp[i][j]=grid[i][j];
+                
+                if(i==n-1 and j==m-1) cur[j]=grid[i][j];
                 else{
                     int l=grid[i][j];
                     if(i+1>=n) l+=1e9;
-                    else l+=dp[i+1][j];
+                    else l+=nxt[j]; // req next row cur col
                     int r=grid[i][j];
                     if(j+1>=m) r+=1e9;
-                    else r+=dp[i][j+1];
-                     dp[i][j]=min(l,r);
+                    else r+=cur[j+1]; // req cur row and next col
+                     cur[j]=min(l,r);
                 }
-                
             }
+            nxt=cur;
         }
-        // cout<<dp[0][2]<<endl;
-        return dp[0][0];
+        return nxt[0];
     }
 };
