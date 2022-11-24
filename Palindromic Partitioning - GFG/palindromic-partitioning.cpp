f07017fbfc3c6a0bc2c66c7625f55d7bb42f9d90
HITESH MEWADA <mewadahitesh00@gmail.com>
2022-11-24 17:39:45 +0530

    Added solution - LeetHub

--- Palindromic Partitioning - GFG/palindromic-partitioning.cpp
+++ Palindromic Partitioning - GFG/palindromic-partitioning.cpp
@@ -17,29 +17,27 @@ public:
         }
         return true;
     }
-    int dp[501];
-    int partition(int st, int end, string str){
-        if(st==end) return 0;
-        int cost=0;
-        string temp;
-        if(dp[st]!=-1) return dp[st];
-        int mnCost=INT_MAX;
-        for(int part=st;part<end;part++){
-            temp+=str[part];
-            if(palin(temp)){
-                if(part!=(end-1))
-                cost=1+partition(part+1,end,str);
-            }
-            mnCost=min(mnCost,cost);
-        }
-        if(palin(temp)) return dp[st]=0;
-        return dp[st]=mnCost;
-    }
     int palindromicPartition(string str)
     {
         // code here
-        memset(dp,-1,sizeof(dp));
-        return partition(0,str.size(),str);
+        int n=str.size();
+        vector<int>dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            int cost=0;
+            string temp;
+            int mnCost=INT_MAX;
+            for(int part=i;part<n;part++){
+                temp+=str[part];
+                if(palin(temp)){
+                    // if(part!=(n-1))
+                    cost=1+dp[part+1];
+                }
+                mnCost=min(mnCost,cost);
+            }
+            if(palin(temp))  dp[i]=0;
+             dp[i]=mnCost;
+        }
+        return dp[0]-1;
     }
 };
 
